cc01be620012aa8b377f227ac4b99adad2b51fc4
vignedev <vignedev@gmail.com>
2019-05-17 20:39:52 +0200

    implement basic argument handling

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 const
+    { red } = require('ansi-colors'),
     crypto = require('crypto-js'),  // Perhaps translate into nodev10 crypto?
     aes = require('crypto-js/aes'),
     fetch = require('node-fetch'),
     { AutoComplete, MultiSelect } = require('enquirer'),
     ProgressBar = require('progress'),
-    { basename } = require('path'),
+    { basename, resolve } = require('path'),
     fs = require('fs'),
-    argv = require('minimist')(process.argv.slice(2))
+    argv = require('minimist')(process.argv.slice(2), {alias: {
+        anime: 'a', episode: 'e', output: 'o'
+    }})
     
 const
     baseUrl = 'https://twist.moe',
     aesKey = "k8B$B@0L8D$tDYHGmRg98sQ7!%GOEGOX27T",
     accessToken = "1rj2vRtegS8Y60B3w3qNZm5T2Q0TN2NR"
 
-;(async() => {
-    const pickedAnime = await (new AutoComplete({
-        name: 'anime', message: 'Pick your anime:', limit: 10,
-        choices: (await getJSON('/api/anime')).map(x=>({name:x.title,value:x}))
-    })).run()
 
-    const sources = (await getJSON(`/api/anime/${pickedAnime.slug.slug}/sources`)).reduce((acc,val) => {
-        acc[`Episode ${val.number}`] = val
-        return acc
-    }, {})
-    
-    const pickedEpisodes = (await (new MultiSelect({ // Choices are broken, they don't read the value field, workaround present
-        name: 'episodes', message: 'Select episodes:', limit: 24,
-        choices: Object.keys(sources)
-    })).run()).map(x=>sources[x])
-    
-    for(let i = 0; i < pickedEpisodes.length; i++){
-        downloadWithFancyProgressbar(decryptSource(pickedEpisodes[i].source), `  Episode ${pickedEpisodes[i].number} (${i+1}/${pickedEpisodes.length}) [:bar] :rate/bps :percent :etas`)
+;(async() => {
+    try{
+        const animeList = await getJSON('/api/anime')
+        const selectedAnime = argv.anime ? animeList.find(x => x.title.toLowerCase().includes(argv.anime.toLowerCase())) : await (new AutoComplete({
+            name: 'anime', message: 'Pick your anime:', limit: 10,
+            choices: animeList.map(x => ({ name: x.title, value: x }))
+        })).run()
+
+        const sourceList = await getJSON(`/api/anime/${selectedAnime.slug.slug}/sources`)
+        const sources = sourceList.reduce((acc, val) => {
+            acc[`Episode ${val.number}`] = val
+            return acc
+        }, {})
+
+        let source = sources[`Episode ${argv.episode}`]
+        if (!source) throw new Error('Episode not available.')
+        const pickedEpisodes = argv.episode ? [source] : (await (new MultiSelect({ // Choices are broken, they don't read the value field, workaround present
+            name: 'episodes', message: 'Select episodes:', limit: 24,
+            choices: Object.keys(sources)
+        })).run()).map(x => sources[x])
+
+        for (let i = 0; i < pickedEpisodes.length; i++) {
+            if(argv.output == '-')
+                await downloadAndPipeIntoStdout(decryptSource(pickedEpisodes[i].source))
+            else
+                await downloadWithFancyProgressbar(decryptSource(pickedEpisodes[i].source), `  Episode ${pickedEpisodes[i].number} (${i + 1}/${pickedEpisodes.length}) [:bar] :rate/bps :percent :etas`)
+        }
+    }catch(err){
+        console.error(red('error: '), err)
     }
 })()
 
@@ -51,10 +66,19 @@ function downloadWithFancyProgressbar(url, text){
             let progress = new ProgressBar(text, {
                 complete: '=', incomplete: '.', width: 24, total: parseInt(res.headers.get('content-length'))
             })
-            const dest = fs.createWriteStream(`${process.cwd()}/${basename(url)}`)
+            const dest = fs.createWriteStream(`${resolve(process.cwd(), argv.output || '')}/${basename(url)}`)
             res.body.pipe(dest)
 
             res.body.on('data', chunk => progress.tick(chunk.length))
+            res.body.on('end', resolve)
+            res.body.on('error', reject)
+        }).catch(reject)
+    })
+}
+function downloadAndPipeIntoStdout(url){
+    return new Promise((resolve,reject) => {
+        fetch(baseUrl + url).then(res => {
+            res.body.pipe(process.stdout)
             res.body.on('end', resolve)
             res.body.on('error', reject)
         }).catch(reject)
